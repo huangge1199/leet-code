a22185ce4c173bfa78d22a5ef1cbfd59857ef858
huangge1199 <huangge1199@hotmail.com>
2021-02-05 13:15:08 +0800

    力扣：230. 二叉搜索树中第K小的元素

---
+++ LeetCode/src/main/java/com/code/leet/study/t20210204/KthSmallest.java
@@ -0,0 +1,25 @@
+package com.code.leet.study.t20210204;
+
+import com.code.leet.entiy.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class KthSmallest {
+
+    public int kthSmallest(TreeNode root, int k) {
+        List<Integer> nums = getList(root, new ArrayList<>());
+        return nums.get(k - 1);
+    }
+
+    private List<Integer> getList(TreeNode root, List<Integer> list) {
+        if(root==null){
+            return list;
+        }
+        getList(root.left,list);
+        list.add(root.val);
+        getList(root.right,list);
+
+        return list;
+    }
+}
