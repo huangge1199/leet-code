c1cc5249296f8aff58f6fe2bbdf6f03a07406604
huangge1199 <huangge1199@hotmail.com>
2021-02-07 15:17:59 +0800

    力扣：剑指 Offer 52. 两个链表的第一个公共节点

--- LeetCode/src/main/java/com/code/leet/study/t20210207/GetIntersectionNode.java
+++ LeetCode/src/main/java/com/code/leet/study/t20210207/GetIntersectionNode.java
@@ -6,6 +6,9 @@ import com.code.leet.entiy.ListNode;
  * 找到两个单链表相交的起始节点。
  */
 public class GetIntersectionNode {
+    /**
+     * 剑指 Offer 52. 两个链表的第一个公共节点
+     */
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         ListNode pA = headA, pB = headB;
         if(pA == null || pB == null){
