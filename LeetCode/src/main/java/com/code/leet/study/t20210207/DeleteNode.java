7055d023887f1b40936248c8971bb15649de3d24
huangge1199 <huangge1199@hotmail.com>
2021-02-07 13:32:26 +0800

    力扣：237. 删除链表中的节点

---
+++ LeetCode/src/main/java/com/code/leet/study/t20210207/DeleteNode.java
@@ -0,0 +1,13 @@
+package com.code.leet.study.t20210207;
+
+import com.code.leet.entiy.ListNode;
+
+/**
+ * 请编写一个函数，使其可以删除某个链表中给定的（非末尾）节点。传入函数的唯一参数为 要被删除的节点 。
+ */
+public class DeleteNode {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
