31ba7d7121e0aea35f4ce12014ee18b639e414fe
huangge1199 <huangge1199@hotmail.com>
2021-02-07 13:46:11 +0800

    力扣：876. 链表的中间结点

---
+++ LeetCode/src/main/java/com/code/leet/study/t20210207/MiddleNode.java
@@ -0,0 +1,22 @@
+package com.code.leet.study.t20210207;
+
+import com.code.leet.entiy.ListNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给定一个头结点为 head 的非空单链表，返回链表的中间结点。
+ * <p>
+ * 如果有两个中间结点，则返回第二个中间结点。
+ */
+public class MiddleNode {
+    public ListNode middleNode(ListNode head) {
+        List<ListNode> list = new ArrayList<>();
+        while (head != null) {
+            list.add(head);
+            head = head.next;
+        }
+        return list.get(list.size() / 2);
+    }
+}
