e7f4e9f1e9e4e859bf187306fb9ba7c9b3bf5cc6
huangge1199 <huangge1199@hotmail.com>
2021-02-09 15:54:39 +0800

    力扣：138. 复制带随机指针的链表

--- LeetCode/src/main/java/com/code/leet/entiy/ListNode.java
+++ LeetCode/src/main/java/com/code/leet/entiy/ListNode.java
@@ -1,5 +1,7 @@
 package com.code.leet.entiy;
 
+import java.util.List;
+
 /**
  * @Author: hyy
  * @Date: 2020-02-12 14:36
@@ -9,7 +11,31 @@ public class ListNode {
     public int val;
     public ListNode next;
 
-    public ListNode(int x) {
-        val = x;
+    public ListNode() {}
+    public ListNode(int val) {
+        this.val = val;
+    }
+    public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+    public ListNode(List<Integer> list){
+        if(list.size()==0){
+            return;
+        }
+        if(list.size()==1){
+            this.val = list.get(0);
+            return;
+        }
+        if(list.size()==2){
+            this.val = list.get(0);
+            this.next = new ListNode(list.get(1));
+            return;
+        }
+        this.val = list.get(0);
+        this.next = new ListNode(list.get(1));
+        ListNode newHead = this.next;
+        for (int i = 2; i < list.size(); i++) {
+            newHead.next = new ListNode(list.get(i));
+            newHead = newHead.next;
+        }
     }
 }
--- LeetCode/src/main/java/com/code/leet/entiy/TreeNode.java
+++ LeetCode/src/main/java/com/code/leet/entiy/TreeNode.java
@@ -1,13 +1,25 @@
 package com.code.leet.entiy;
 
+
 /**
  * @Author: hyy
  * @Date: 2020-02-13 18:25
  */
-
 public class TreeNode {
-    public int val;
     public TreeNode left;
+    public Integer val;
     public TreeNode right;
-    public TreeNode(int x) { val = x; }
+
+    TreeNode() {
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
 }
