8b2979d1a439d24d7eab43798700be2df652c1ee
huangge1199 <huangge1199@hotmail.com>
2021-03-19 13:15:01 +0800

    Node 变更

--- LeetCode/src/main/java/com/code/leet/entiy/Node.java
+++ LeetCode/src/main/java/com/code/leet/entiy/Node.java
@@ -4,10 +4,18 @@ public class Node {
     public int val;
     public Node next;
     public Node random;
+    public Node prev;
+    public Node child;
 
     public Node(int val) {
         this.val = val;
         this.next = null;
         this.random = null;
+    }
+
+    public Node(int val, Node prev, Node head, Node next) {
+        head.val = val;
+        head.prev = prev;
+        head.next = next;
     }
 }
