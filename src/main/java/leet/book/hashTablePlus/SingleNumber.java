55391ce72530874d878108070acce8b8bc6cf459
huangge1199@hotmail.com <huangge1199>
2021-07-04 18:06:36 +0800

    LeetBook--哈希表--只出现一次的数字

---
+++ src/main/java/leet/book/hashTablePlus/SingleNumber.java
@@ -0,0 +1,12 @@
+package leet.book.hashTablePlus;
+
+public class SingleNumber {
+    class Solution {
+        public int singleNumber(int[] nums) {
+            for (int i = 1; i < nums.length; i++) {
+                nums[0] ^= nums[i];
+            }
+            return nums[0];
+        }
+    }
+}
