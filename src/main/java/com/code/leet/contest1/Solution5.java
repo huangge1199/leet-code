f9d7b13972f3ded322f5335bfef91da3637267b2
huangge1199@hotmail.com <huangge1199>
2021-07-11 22:23:40 +0800

    训练营周赛1

---
+++ src/main/java/com/code/leet/contest1/Solution5.java
@@ -0,0 +1,31 @@
+package com.code.leet.contest1;
+
+public class Solution5 {
+    public static void main(String[] args) {
+        Solution5 solution = new Solution5();
+        System.out.println(solution.solve("abccaba"));
+    }
+
+    public int solve(String tmp) {
+//        return dfs(tmp, "abcba");
+        return 0;
+    }
+
+    private int dfs(String str, String target) {
+        if (str.equals(target)) {
+            return 1;
+        }
+        if (target.equals("")) {
+            return 0;
+        }
+        int index = str.indexOf(target.charAt(0));
+        int count = 0;
+        while (index >= 0) {
+            str = str.substring(index + 1);
+            count += dfs(str, target.substring(1));
+            count %= 1000000007;
+            index = str.indexOf(target.charAt(0));
+        }
+        return count;
+    }
+}
---
+++ src/main/java/com/code/leet/contest1/Solution6.java
@@ -0,0 +1,84 @@
+package com.code.leet.contest1;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Solution6 {
+    public static void main(String[] args) {
+        Solution6 solution = new Solution6();
+        int[] nums;
+        int[] nums1;
+        for (int i = 2; i < 100; i++) {
+            nums = new int[i];
+            for (int j = 0; j < nums.length; j++) {
+                nums[j] = j;
+            }
+            nums1 = Arrays.copyOf(nums, nums.length);
+            int index = 1;
+            while (true) {
+                nums1 = solution.change(nums1);
+                if (solution.bl(nums, nums1)) {
+                    System.out.println(i + ":" + index);
+                    break;
+                }
+                index++;
+            }
+            i++;
+        }
+    }
+
+    private boolean bl(int[] nums1, int[] nums2) {
+        for (int i = 0; i < nums1.length; i++) {
+            if (nums1[i] != nums2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int solve(int[] nums, int m, int kth) {
+        int[] nums1 = Arrays.copyOf(nums, nums.length);
+        int index = -1;
+        for (int i = 0; i < m; i++) {
+            nums = change(nums);
+            if (bl(nums1, nums)) {
+                index = i + 1;
+                break;
+            }
+        }
+        if (index > 0) {
+            for (int i = 0; i < m % index; i++) {
+                nums = change(nums);
+                if (bl(nums1, nums)) {
+                    index = i + 1;
+                    break;
+                }
+            }
+        }
+        return nums[kth - 1];
+    }
+
+    private int[] change(int[] nums) {
+        Stack<Integer> stack = new Stack<>();
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (i % 2 == 0) {
+                stack.push(nums[i]);
+            } else {
+                queue.add(nums[i]);
+            }
+        }
+        int index = 0;
+        while (!queue.isEmpty()) {
+            nums[index] = queue.poll();
+            index++;
+        }
+        while (!stack.isEmpty()) {
+            nums[index] = stack.pop();
+            index++;
+        }
+        return nums;
+    }
+}
