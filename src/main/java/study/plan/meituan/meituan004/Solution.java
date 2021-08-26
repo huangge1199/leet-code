d6c979e526ee941d784409d45281fd7d5ad718d6
huangge1199 <huangge1199@hotmail.com>
2021-08-26 16:07:26 +0800

    meituan-004. 小团的复制粘贴

--- src/main/java/study/plan/meituan/meituan004/Solution.java
+++ src/main/java/study/plan/meituan/meituan004/Solution.java
@@ -1,12 +1,16 @@
-package study.plan.meituan;
+package study.plan.meituan.meituan004;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Solution {
 
     static Scanner sc;
     static PrintWriter pw;
+
     public static void main(String[] args) throws IOException {
         sc = new Scanner(System.in);
         pw = new PrintWriter(System.out);
@@ -15,7 +19,34 @@ public class Solution {
     }
 
     public static void solve() throws IOException {
-
+        int length = sc.nextInt();
+        int[] bArr = new int[length];
+        Arrays.fill(bArr, -1);
+        int[] aArr = sc.nextIntArr(length);
+        int num = sc.nextInt();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < num; i++) {
+            int op = Integer.parseInt(sc.next());
+            if (op == 2) {
+                list.add(bArr[Integer.parseInt(sc.next()) - 1]);
+            } else {
+                int k = Integer.parseInt(sc.next());
+                int x = Integer.parseInt(sc.next()) - 1;
+                int y = Integer.parseInt(sc.next()) - 1;
+//                System.arraycopy(aArr, x, bArr, y, k);
+                for (int j = 0; j < k; j++) {
+                    if (y >= bArr.length || x >= aArr.length) {
+                        break;
+                    }
+                    bArr[y] = aArr[x];
+                    x++;
+                    y++;
+                }
+            }
+        }
+        for (Integer integer : list) {
+            pw.println(integer);
+        }
     }
 
 
