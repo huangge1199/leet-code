0728284b255b676c32b6131d51689c11cfaced2f
huangge1199 <huangge1199@hotmail.com>
2021-08-31 11:36:23 +0800

    1109:航班预订统计

---
+++ src/main/java/leetcode/editor/cn/CorporateFlightBookings.java
@@ -0,0 +1,73 @@
+//这里有 n 个航班，它们分别从 1 到 n 进行编号。 
+//
+// 有一份航班预订表 bookings ，表中第 i 条预订记录 bookings[i] = [firsti, lasti, seatsi] 意味着在从 
+//firsti 到 lasti （包含 firsti 和 lasti ）的 每个航班 上预订了 seatsi 个座位。 
+//
+// 请你返回一个长度为 n 的数组 answer，其中 answer[i] 是航班 i 上预订的座位总数。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：bookings = [[1,2,10],[2,3,20],[2,5,25]], n = 5
+//输出：[10,55,45,25,25]
+//解释：
+//航班编号        1   2   3   4   5
+//预订记录 1 ：   10  10
+//预订记录 2 ：       20  20
+//预订记录 3 ：       25  25  25  25
+//总座位数：      10  55  45  25  25
+//因此，answer = [10,55,45,25,25]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：bookings = [[1,2,10],[2,2,15]], n = 2
+//输出：[10,25]
+//解释：
+//航班编号        1   2
+//预订记录 1 ：   10  10
+//预订记录 2 ：       15
+//总座位数：      10  25
+//因此，answer = [10,25]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= n <= 2 * 10⁴ 
+// 1 <= bookings.length <= 2 * 10⁴ 
+// bookings[i].length == 3 
+// 1 <= firsti <= lasti <= n 
+// 1 <= seatsi <= 10⁴ 
+// 
+// Related Topics 数组 前缀和 👍 218 👎 0
+
+package leetcode.editor.cn;
+//1109:航班预订统计
+class CorporateFlightBookings{
+    public static void main(String[] args) {
+        //测试代码
+        Solution solution = new CorporateFlightBookings().new Solution();
+    }
+    
+    //力扣代码
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] corpFlightBookings(int[][] bookings, int n) {
+        int[] result = new int[n];
+        for (int[] booking : bookings) {
+            for (int j = booking[0] - 1; j < booking[1]; j++) {
+                result[j] += booking[2];
+            }
+        }
+        return result;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
