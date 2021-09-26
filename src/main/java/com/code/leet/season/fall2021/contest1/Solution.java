79044953888d1b9611bc20382951b823324f4cbb
huangge1199@hotmail.com <huangge1199>
2021-09-26 08:28:59 +0800

    2021秋季赛

---
+++ src/main/java/com/code/leet/season/fall2021/contest1/Solution.java
@@ -0,0 +1,38 @@
+package com.code.leet.season.fall2021.contest1;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @description:
+ * @author: Administrator
+ * @date: 2021/9/25-15:15
+ */
+public class Solution {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    Set<Integer> set = new HashSet<>();
+
+    public int numColor(TreeNode root) {
+        dfs(root);
+        return set.size();
+    }
+
+    private void dfs(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        set.add(root.val);
+        dfs(root.left);
+        dfs(root.right);
+    }
+}
