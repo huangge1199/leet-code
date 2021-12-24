61ddc5b0d69b230bcb2cf00e9e14a250deeeb717
轩辕龙儿 <hyy>
2021-12-24 17:49:20 +0800

    完善

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import cn.hutool.core.io.file.FileWriter;
 import cn.hutool.http.HttpRequest;
+import cn.hutool.json.JSONArray;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 
@@ -11,10 +12,52 @@ public class Main {
                 .timeout(20000)
                 .execute().body();
         JSONObject jsonObject = JSONUtil.parseObj(result);
-        JSONObject chapterList = jsonObject.getJSONObject("data");
-        FileWriter writer = new FileWriter("test.txt");
-        writer.write("test");
+        JSONArray chapterList = jsonObject.getJSONObject("data").getJSONArray("outlineVOList");
+        FileWriter writer = new FileWriter("D:\\project\\leet-code\\src\\main\\java\\test.txt");
+        for (int i = 0; i < chapterList.size(); i++) {
+            JSONObject chaps = chapterList.getJSONObject(i);
+            writer.append("第" + (i + 1) + "章：" + chaps.getStr("title") + "\r\n");
+            JSONArray chapters = chaps.getJSONArray("chapterList");
+            if (chapters == null) {
+                continue;
+            }
+            for (int j = 0; j < chapters.size(); j++) {
+                JSONObject sections = chapters.getJSONObject(j);
+                writer.append("\t第" + (i + 1) + "-" + (j + 1) + "节：" + sections.getStr("title") + "\r\n");
+                JSONArray sets = sections.getJSONArray("sectionDetailList");
+                if (sets == null) {
+                    continue;
+                }
+                for (int k = 0; k < sets.size(); k++) {
+                    writer.append("\t\t"
+                            + (k + 1) + "."
+                            + sets.getJSONObject(k).getStr("title")
+                            + "\t"
+                            + getDurats(sets.getJSONObject(k).getInt("duration"))
+                            + "\r\n");
+                }
+            }
+        }
+    }
 
-        System.out.println("--------");
+    private static String getDurats(Integer time) {
+        String str = "";
+        if (time == null) {
+            return str;
+        }
+        if (time > 60) {
+            str = time % 60 + "秒";
+            time /= 60;
+            if (time > 60) {
+                str = time % 60 + "分" + str;
+                time /= 60;
+                str = time + "时" + str;
+            } else {
+                str = time % 60 + "分" + str;
+            }
+        } else {
+            str = time % 60 + "秒";
+        }
+        return str;
     }
 }
